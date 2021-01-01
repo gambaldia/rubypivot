9122fe3e8f966016603ef3cf518bce38a91e6d38
hiro utsumi <gambaldia@gmail.com>
2021-01-01 11:42:45 +0100

    ver 0.0.2 fix No.1

--- lib/rubypivot.rb
+++ lib/rubypivot.rb
@@ -1,10 +1,10 @@
 # require "rubypivot/version"
+require "rubypivot/version"
 require "rubypivot/pivot"
 require "rubypivot/pivot_row"
 require "rubypivot/spread_table"
 require "rubypivot/html_tag"
 
 module Rubypivot
-  VERSION = "0.0.2"
   class PivotError < StandardError; end
 end
